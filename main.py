4143623eef7062451a3b759442e4d3006cdfdd6d
Bálint Dániel <balint.daniel623@gmail.com>
2023-07-01 15:14:46 +0200

    train random weights and biases

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from nnfs.datasets import spiral_data
+from nnfs.datasets import spiral_data, vertical_data
+
 import matplotlib.pyplot as plt
 import nnfs
 import numpy as np
@@ -12,32 +13,51 @@ nnfs.init()
 
 
 
-X, y = spiral_data(samples=100, classes=3)
-# print("X shape", X.shape)
+X, y = vertical_data(samples=100, classes=3)
 # plt.scatter(X[:,0], X[:,1], c=y, cmap="brg")
 # plt.show()
 
-
-dense1 = Layer_Dense(2, 3)
+dense1 = Layer_Dense(2, 3) # Input, Output
 activation1 = Activation_ReLu()
-dense2 = Layer_Dense(3, 3)
+dense2 = Layer_Dense(3, 2)
 activation2 = Activation_Softmax()
+
 loss_function = Loss_CategoricalCrossentropy()
 
-dense1.forward(X)
-activation1.forward(dense1.output)
-dense2.forward(activation1.output)
-activation2.forward(dense2.output)
-
-print("Output:", activation2.output[:5])
-
-loss = loss_function.calculate(activation2.output, y)
-print("Loss:", loss)
+# Save values
+lowest_loss = 9999999
+best_dense1_weights = dense1.weights.copy()
+best_dense1_biases = dense1.biases.copy()
+best_dense2_weights = dense2.weights.copy()
+best_dense2_biases = dense2.biases.copy()
 
 
-# TODO: Place this somewhere
-predictions = np.argmax(activation2.output, axis=1)
-if len(y.shape) == 2:
-    y = np.argmax(y, axis=1)
-accuracy = np.mean(predictions==y)
-print("Accuracy:", accuracy)
+# Try options randomly
+for iteration in range(10000):
+    dense1.weights = 0.05 * np.random.randn(2, 3)
+    dense1.biases = 0.05 * np.random.randn(1, 3)
+    dense2.weights = 0.05 * np.random.randn(3, 3)
+    dense2.biases = 0.05 * np.random.randn(1, 3)
+
+    # Traning dat throw layer
+    dense1.forward(X)
+    activation1.forward(dense1.output)
+    dense2.forward(activation1.output)
+    activation2.forward(dense2.output)
+
+    # Calculate loss
+    loss = loss_function.calculate(activation2.output, y)
+
+    # Calculate accuracy
+    predictions = np.argmax(activation2.output, axis=1)
+    accuray = np.mean(predictions == y)
+
+    if loss < lowest_loss:
+        print(f"Network improved. Accuracy: {accuray}, Loss: {loss}")
+        best_dense1_weights = dense1.weights.copy()
+        best_dense1_biases = dense1.biases.copy()
+        best_dense2_weights = dense2.weights.copy()
+        best_dense2_biases = dense2.biases.copy()
+
+        lowest_loss = loss
+

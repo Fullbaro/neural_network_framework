aef5952df03ac4ff63c85a6026f77980224a1b2d
Bálint Dániel <balint.daniel623@gmail.com>
2023-07-01 07:20:17 +0200

    save

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import numpy as np
 
 inputs = [1, 2, 3, 2.5]
-weights = [0.2, 0.8, -0.5, 1.0]
-bias = 2
+weights = [
+    [0.2, 0.8, -0.5, 1.0],
+    [0.5, -0.91, 0.26, -0.5],
+    [-0.26, -0.27, 0.17, 0.87]
+]
+
+biases = [2, 3, 0.5]
 
 
-outputs = np.dot(inputs, weights) + bias  # * Dot product. Elementvise operation
+layer_outputs = np.dot(weights, inputs) + biases
 
-print(outputs)
+print(layer_outputs)

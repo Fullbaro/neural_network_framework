f495819916e680bf00fcb6524707e2d642b444e0
Bálint Dániel <balint.daniel623@gmail.com>
2023-07-01 12:09:52 +0200

    input and one dense layer

--- main.py
+++ main.py
@@ -5,35 +5,24 @@ import numpy as np
 
 nnfs.init()
 
+class Layer_Dense:
+
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons) # Random init
+        self.biases = np.zeros((1, n_neurons)) # Init to 0
+        print(f"Weights: {self.weights}\nBiases: {self.biases}\n")
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
 X, y = spiral_data(samples=100, classes=3)
-plt.scatter(X[:,0], X[:,1], c=y, cmap="brg")
-plt.show()
+# print("X shape", X.shape)
+# plt.scatter(X[:,0], X[:,1], c=y, cmap="brg")
+# plt.show()
 
 
+dense = Layer_Dense(2, 3)
+dense.forward(X)
 
-# inputs = [
-#     [1, 2, 3, 2.5],
-#     [2.0, 5.0, -1.0, 2.0],
-#     [-1.5, 2.7, 3.3, -0.8],
-# ]
-
-# weights = [
-#     [0.2, 0.8, -0.5, 1.0],
-#     [0.5, -0.91, 0.26, -0.5],
-#     [-0.26, -0.27, 0.17, 0.87]
-# ]
-# biases = [2, 3, 0.5]
-
-# weights2 = [
-#     [0.1, -0.14, 0.5],
-#     [-0.5, 0.12, -0.33],
-#     [-0.44, 0.73, -0.13]
-# ]
-# biases2 = [-1, 2, -0.5]
-
-
-
-# layer1_putptus = np.dot(inputs, np.array(weights).T) + biases
-# layer2_putptus = np.dot(layer1_putptus, np.array(weights2).T) + biases2
-
-# print(layer2_putptus)
+print(dense.output[:10])

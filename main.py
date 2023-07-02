ed08d15cd091321555d5629ff8347a5cd80e82f0
Bálint Dániel <balint.daniel623@gmail.com>
2023-07-02 15:41:13 +0200

    Classification
    dropout works

--- main.py
+++ main.py
@@ -13,24 +13,23 @@ from model import *
 
 nnfs.init()
 
-X, y = spiral_data(samples=100, classes=2)
-X_test, y_test = spiral_data(samples=100, classes=2)
+X, y = spiral_data(samples=100, classes=3)
+X_test, y_test = spiral_data(samples=100, classes=3)
 
-y = y.reshape(-1, 1)
-y_test = y_test.reshape(-1, 1)
 
 model = Model()
 
 model.add(Layer_Dense(2, 64, weight_regularizer_l2=5e-4, bias_regularizer_l2=5e-4))
 model.add(Activation_ReLU())
-model.add(Layer_Dense(64, 1))
-model.add(Activation_Sigmoid())
+model.add(Layer_Dropout(0.1))
+model.add(Layer_Dense(64, 3))
+model.add(Activation_Softmax())
 
 
 model.set(
-    loss=Loss_BinaryCrossentropy(),
-    optimizer=Optimizer_Adam(decay=5e-7),
-    accuracy=Accuracy_Regression()
+    loss=Loss_CategoricalCrossentropy(),
+    optimizer=Optimizer_Adam(learning_rate=0.05, decay=5e-5),
+    accuracy=Accuracy_Categorical()
 )
 
 model.finalize()

0cb9d3c9dc032563e9327bb558b61874f546e45f
Bálint Dániel <balint.daniel623@gmail.com>
2023-07-03 20:17:34 +0200

    save

---
+++ lib/dataset.py
@@ -0,0 +1,98 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+class ImageClassificationDataset:
+
+    def __init__(self):
+        self.path: str
+        self.labels: np.array
+        self.X: np.array
+        self.y: np.array
+
+    def load(self, path):
+        self.path = path
+        self.labels = np.array(os.listdir(self.path))
+
+        X_list = []
+        y_list = []
+        for label in self.labels:
+            for file in os.listdir(os.path.join(self.path, label)):
+                X_list.append(cv2.imread(os.path.join(self.path, label, file), cv2.IMREAD_UNCHANGED))
+                y_list.append(label)
+
+        self.X = np.array(X_list)
+        self.y = np.array(y_list).astype('uint8')
+
+    def preprocess(self, grayscale=False, white_balance=False, *, size):
+        for i in range(len(self.X)):
+            img = self.X[i]
+
+            if white_balance and len(img.shape) == 3:
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+                avg_a = np.average(img[:, :, 1])
+                avg_b = np.average(img[:, :, 2])
+                img[:, :, 1] = img[:, :, 1] - ((avg_a - 128) * (img[:, :, 0] / 255.0) * 1.1)
+                img[:, :, 2] = img[:, :, 2] - ((avg_b - 128) * (img[:, :, 0] / 255.0) * 1.1)
+                img = cv2.cvtColor(img, cv2.COLOR_LAB2BGR)
+
+            if grayscale and len(img.shape) == 3:
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            if size is not None:
+                self.size = size
+                img = cv2.resize(img, (size, size))
+
+            self.X[i] = img
+
+    def augment(self):
+        pass
+
+    def shuffle(self):
+        keys = np.array(range(self.X.shape[0]))
+        np.random.shuffle(keys)
+        self.X = self.X[keys]
+        self.y = self.y[keys]
+
+    def normalize(self):
+        self.X = (self.X.astype(np.float32) - 127.5) / 127.5
+
+    def reshape(self):
+        self.X = self.X.reshape(self.X.shape[0], -1)
+
+    def split(self, valid=0.2, test=0.1):
+        dataset_size = self.X.shape[0]
+        num_train = int(dataset_size * (1 - valid - test))
+        num_valid = int(dataset_size * valid)
+        num_test = int(dataset_size * test)
+
+        self.X_train = self.X[:num_train]
+        self.y_train = self.y[:num_train]
+        self.X_valid = self.X[num_train:num_train + num_valid]
+        self.y_valid = self.y[num_train:num_train + num_valid]
+        self.X_test = self.X[num_train + num_valid:num_train + num_valid + num_test]
+        self.y_test = self.y[num_train + num_valid:num_train + num_valid + num_test]
+
+        return self.X_train, self.y_train, self.X_valid, self.y_valid, self.X_test, self.y_test
+
+
+    def preview(self):
+        print(f"Train batch contains {len(self.X_train):_} images")
+        print(f"Validation batch contains {len(self.X_valid):_} images")
+        print(f"Test batch contains {len(self.X_test):_} images")
+
+        fig, axs = plt.subplots(5, 5, figsize=(10, 10))
+        fig.subplots_adjust(hspace=0.3, wspace=0)
+
+        for i, ax in enumerate(axs.flat):
+            img = self.X[i].reshape((self.size, self.size))
+
+            if len(img.shape) == 2:
+                ax.imshow(img, cmap='gray')
+            else:
+                ax.imshow(img)
+            ax.set_title(self.y[i])
+            ax.axis('off')
+
+        plt.show()
---
+++ lib/model.py
@@ -0,0 +1,270 @@
+import copy
+import pickle
+from lib.layers import *
+from lib.activations import *
+from lib.metrics import *
+
+class Model:
+
+    def __init__(self):
+        self.layers = []
+        self.softmax_classifier_output = None
+
+    def add(self, layer):
+        self.layers.append(layer)
+
+    def set(self, *, loss=None, optimizer=None, accuracy=None):
+        if loss is not None:
+            self.loss = loss
+        if optimizer is not None:
+            self.optimizer = optimizer
+        if accuracy is not None:
+            self.accuracy = accuracy
+
+    def evaluate(self, X_val, y_val, *, batch_size=None):
+        validation_steps = 1
+
+        if batch_size is not None:
+            validation_steps = len(X_val) // batch_size
+
+            if validation_steps * batch_size < len(X_val):
+                validation_steps += 1
+
+        self.loss.new_pass()
+        self.accuracy.new_pass()
+
+        for step in range(validation_steps):
+
+            if batch_size is None:
+                batch_X = X_val
+                batch_y = y_val
+            else:
+                batch_X = X_val[step*batch_size:(step+1)*batch_size]
+                batch_y = y_val[step*batch_size:(step+1)*batch_size]
+
+            output = self.forward(batch_X, training=False)
+            self.loss.calculate(output, batch_y)
+
+            predictions = self.output_layer_activation.predictions(
+            output)
+            self.accuracy.calculate(predictions, batch_y)
+
+        validation_loss = self.loss.calculate_accumulated()
+        validation_accuracy = self.accuracy.calculate_accumulated()
+
+        print(f'Evaluation, ' +
+            f'acc: {validation_accuracy:.3f}, ' +
+            f'loss: {validation_loss:.3f}'
+        )
+
+    def train(self, X, y, *, epochs=1, batch_size=None, print_every=1, validation_data=None):
+        self.accuracy.init(y)
+
+        train_steps = 1
+
+        if validation_data is not None:
+            validation_steps = 1
+            X_val, y_val = validation_data
+
+        if batch_size is not None:
+            train_steps = len(X) // batch_size
+            if train_steps * batch_size < len(X):
+                train_steps += 1
+
+            if validation_data is not None:
+                validation_steps = len(X_val) // batch_size
+                if validation_steps * batch_size < len(X_val):
+                    validation_steps += 1
+
+
+        for epoch in range(1, epochs+1):
+            print(f'epoch: {epoch}')
+
+            self.loss.new_pass()
+            self.accuracy.new_pass()
+
+            for step in range(train_steps):
+                if batch_size is None:
+                    batch_X = X
+                    batch_y = y
+                else:
+                    batch_X = X[step*batch_size:(step+1)*batch_size]
+                    batch_y = y[step*batch_size:(step+1)*batch_size]
+
+                output = self.forward(batch_X, training=True)
+                data_loss, regularization_loss = self.loss.calculate(output, batch_y, include_regularization=True)
+                loss = data_loss + regularization_loss
+
+                predictions = self.output_layer_activation.predictions(output)
+                accuracy = self.accuracy.calculate(predictions, batch_y)
+
+                self.backward(output, batch_y)
+
+                self.optimizer.pre_update_params()
+                for layer in self.trainable_layers:
+                    self.optimizer.update_params(layer)
+                self.optimizer.post_update_params()
+
+                if not step % print_every or step == train_steps - 1:
+                    print(f'    step: {step}, ' +
+                    f'acc: {accuracy:.3f}, ' +
+                    f'loss: {loss:.3f} (' +
+                    f'data_loss: {data_loss:.3f},' +
+                    f'reg_loss: {regularization_loss:.3f}), ' +
+                    f'lr: {self.optimizer.current_learning_rate}')
+
+            epoch_data_loss, epoch_regularization_loss = self.loss.calculate_accumulated(include_regularization=True)
+            epoch_loss = epoch_data_loss + epoch_regularization_loss
+            epoch_accuracy = self.accuracy.calculate_accumulated()
+
+            print(f'training, ' +
+                f'acc: {epoch_accuracy:.3f}, ' +
+                f'loss: {epoch_loss:.3f} (' +
+                f'data_loss: {epoch_data_loss:.3f}, ' +
+                f'reg_loss: {epoch_regularization_loss:.3f}), ' +
+                f'lr: {self.optimizer.current_learning_rate}')
+
+            if validation_data is not None:
+                self.loss.new_pass()
+                self.accuracy.new_pass()
+
+                for step in range(validation_steps):
+                    if batch_size is None:
+                        batch_X = X_val
+                        batch_y = y_val
+                    else:
+                        batch_X = X_val[step*batch_size:(step+1)*batch_size]
+                        batch_y = y_val[step*batch_size:(step+1)*batch_size]
+
+                    output = self.forward(batch_X, training=False)
+
+                    self.loss.calculate(output, batch_y)
+
+                    predictions = self.output_layer_activation.predictions(output)
+
+                    self.accuracy.calculate(predictions, batch_y)
+
+                validation_loss = self.loss.calculate_accumulated()
+                validation_accuracy = self.accuracy.calculate_accumulated()
+
+                print(f'validation, ' +
+                    f'acc: {validation_accuracy:.3f},' +
+                    f'loss: {validation_loss:.3f}' +
+                    '\n'
+                )
+
+    def finalize(self):
+        self.input_layer = Layer_Input()
+        layer_count = len(self.layers)
+        self.trainable_layers = []
+
+        for i in range(layer_count):
+            if i == 0:
+                self.layers[i].prev = self.input_layer
+                self.layers[i].next = self.layers[i+1]
+            elif i < layer_count - 1:
+                self.layers[i].prev = self.layers[i-1]
+                self.layers[i].next = self.layers[i+1]
+            else:
+                self.layers[i].prev = self.layers[i-1]
+                self.layers[i].next = self.loss
+                self.output_layer_activation = self.layers[i]
+
+            if hasattr(self.layers[i], 'weights'):
+                self.trainable_layers.append(self.layers[i])
+
+            if self.loss is not None:
+                self.loss.remember_trainable_layers(self.trainable_layers)
+
+        if isinstance(self.layers[-1], Activation_Softmax) and isinstance(self.loss, Loss_CategoricalCrossentropy):
+            self.softmax_classifier_output = Activation_Softmax_Loss_CategoricalCrossentropy()
+
+    def forward(self, X, training):
+        self.input_layer.forward(X, training)
+
+        for layer in self.layers:
+            layer.forward(layer.prev.output, training)
+
+        return layer.output
+
+    def backward(self, output, y):
+        if self.softmax_classifier_output is not None:
+            self.softmax_classifier_output.backward(output, y)
+            self.layers[-1].dinputs = self.softmax_classifier_output.dinputs
+
+            for layer in reversed(self.layers[:-1]):
+                layer.backward(layer.next.dinputs)
+
+            return
+
+        self.loss.backward(output, y)
+
+        for layer in reversed(self.layers):
+            layer.backward(layer.next.dinputs)
+
+    def get_parameters(self):
+        parameters = []
+        for layer in self.trainable_layers:
+            parameters.append(layer.get_parameters())
+
+        return parameters
+
+    def set_parameters(self,parameters):
+        for parameter_set, layer in zip(parameters, self.trainable_layers):
+            layer.set_parameters(*parameter_set)
+
+    def save_parameters(self, path):
+        with open(path, 'wb') as file:
+            pickle.dump(self.get_parameters(), file)
+
+    def load_parameters(self, path):
+        with open(path, 'rb') as file:
+            self.set_parameters(pickle.load(file))
+
+    def save(self, path):
+        model = copy.deepcopy(self)
+
+        model.loss.new_pass()
+        model.accuracy.new_pass()
+
+        model.input_layer.__dict__.pop("output", None)
+        model.loss.__dict__.pop('dinputs', None)
+
+        for layer in model.layers:
+            for property in ['inputs', 'output', 'dinputs', 'dweights', 'dbiases']:
+                layer.__dict__.pop(property, None)
+
+        with open(path, 'wb') as file:
+            pickle.dump(model, file)
+
+    @staticmethod
+    def load(path):
+        with open(path, 'rb') as file:
+            model = pickle.load(file)
+
+        return model
+
+    def predict(self, X, *, bacth_size=None):
+        predictions_steps = 1
+
+        if bacth_size is not None:
+            predictions_steps = len(X) // bacth_size
+
+            if predictions_steps * bacth_size < len(X):
+                predictions_steps += 1
+
+        output = []
+        for step in range(predictions_steps):
+            if bacth_size is None:
+                bacth_X = X
+            else:
+                bacth_X = X[step * bacth_size:(step + 1) * bacth_size]
+
+            bacth_output = self.forward(bacth_X, training=False)
+
+            output.append(bacth_output)
+
+        confidences =  np.vstack(output)
+        predictions = self.output_layer_activation.predictions(confidences)
+
+        return predictions
